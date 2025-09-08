dd3b6fc166130a713845a20b674958549ab5caa3
nayanramam <nayanramam1@gmail.com>
2025-09-08 18:08:47 -0400

    goldschmidt

--- software_test/div_algorithms.py
+++ software_test/div_algorithms.py
@@ -1,8 +1,9 @@
-from math import trunc
+from math import inf, trunc
 
 # Precompute a small lookup table for the reciprocal initial guess in [1, 2)
 # The table stores approximations to 1/x for representative points across [1, 2)
 _RECIP_LUT = [1.0 / (1.0 + (i + 0.5) / 16.0) for i in range(16)]
+print(_RECIP_LUT)
 
 def _initial_guess_from_lut(denominator: int) -> float:
     if denominator <= 0:
@@ -47,4 +48,36 @@ def div_nr(N, D, G=None):
 
     return Q, R
 
-print(div_nr(24, 6))
+# print(div_nr(24, 6))
+
+
+def div_gs(N, D):
+
+    N_prev = N
+    D_prev = D
+    iter = 0
+    num_shifts = 0
+
+    while D > 1:
+        D = D >> 1
+        num_shifts += 1
+
+
+    F = 2 - D
+
+    N_new = inf
+    D_new = inf
+
+    while abs(N_new - N_prev) > N/100000:
+        N_new = N_prev * F
+        D_new = D_prev * F
+
+        F = 2 - D_new
+        N_prev = N_new
+        D_prev = D_new
+        iter += 1
+
+    Q = N_new << num_shifts
+    R = N_new - D_new * Q
+
+    return Q, R
