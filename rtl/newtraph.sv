9b828efdd46e462240812d84816c2eb69990601b
nayanramam <nayanramam1@gmail.com>
2025-10-27 18:04:23 -0400

    newtraph v2 (lut added)

--- rtl/newtraph.sv
+++ rtl/newtraph.sv
@@ -35,8 +35,9 @@ module gcd
 
     logic [WIDTH-1:0] n_reg, d_reg, next_n, next_d;
     logic [WIDTH-1:0] g_prev, g_new;
-    logic [WIDTH-1:0] change, new_change;
+    logic [WIDTH-1:0] change, next_change;
     logic flip, next_flip;
+    logic [31:0] LUT [0:32];
 
     always_ff @(posedge clk_i) begin
         if (rst_i) begin
@@ -51,7 +52,7 @@ module gcd
             n_reg <= next_n;
             d_reg <= next_d;
             g_prev <= g_new;
-            change <= new_change;
+            change <= next_change;
             flip <= next_flip;
         end
     end
@@ -64,7 +65,7 @@ module gcd
         r_o = '0;
         error_o = 2'b00;
         g_new = g_prev;
-        new_change = change;
+        next_change = change;
         next_n = n_reg;
         next_d = d_reg;
         next_flip = flip;
@@ -134,14 +135,16 @@ module gcd
             end
 
             DIV_START: begin
-                //TODO add LUT
-                g_new = 0.5;   // temp until add LUT
+
+                g_new = msb_index(d_reg);
+                g_new = LUT[g_new];
+
                 next_change = '1;
                 next_state = DIV_BRANCH;
             end
 
             DIV_BRANCH: begin
-                if (change > 0.001) begin // TODO change
+                if (change > 0.001) begin
                     next_state = DIV_ITER;
                 end else begin
                     next_state = DIV_FIN;
@@ -150,7 +153,7 @@ module gcd
 
             DIV_ITER: begin
                 g_new = g_prev * (2-(d_reg*g_prev));
-                new_change = ((g_new - g_prev) < 0) ? g_prev - g_new : g_new - g_prev; // abs val of change
+                next_change = ((g_new - g_prev) < 0) ? g_prev - g_new : g_new - g_prev; // abs val of change
                 next_state = DIV_BRANCH;
             end
 
@@ -179,9 +182,54 @@ module gcd
                 r_o = '0;
                 error_o = 2'b11;  // Unknown state error
                 g_new = '0;
-                new_change = '0;
+                next_change = '0;
             end
         endcase
     end
+
+    initial begin
+        LUT[0]  = 1.0;
+        LUT[1]  = 0.5;
+        LUT[2]  = 0.25;
+        LUT[3]  = 0.125;
+        LUT[4]  = 0.0625;
+        LUT[5]  = 0.03125;
+        LUT[6]  = 0.015625;
+        LUT[7]  = 0.0078125;
+        LUT[8]  = 0.00390625;
+        LUT[9]  = 0.001953125;
+        LUT[10] = 0.0009765625;
+        LUT[11] = 0.00048828125;
+        LUT[12] = 0.000244140625;
+        LUT[13] = 0.0001220703125;
+        LUT[14] = 0.00006103515625;
+        LUT[15] = 0.000030517578125;
+        LUT[16] = 0.0000152587890625;
+        LUT[17] = 0.00000762939453125;
+        LUT[18] = 0.000003814697265625;
+        LUT[19] = 0.0000019073486328125;
+        LUT[20] = 0.00000095367431640625;
+        LUT[21] = 0.000000476837158203125;
+        LUT[22] = 0.0000002384185791015625;
+        LUT[23] = 0.00000011920928955078125;
+        LUT[24] = 0.000000059604644775390625;
+        LUT[25] = 0.0000000298023223876953125;
+        LUT[26] = 0.00000001490116119384765625;
+        LUT[27] = 0.000000007450580596923828125;
+        LUT[28] = 0.0000000037252902984619140625;
+        LUT[29] = 0.00000000186264514923095703125;
+        LUT[30] = 0.000000000931322574615478515625;
+        LUT[31] = 0.0000000004656612873077392578125;
+        LUT[32] = 0.00000000023283064365386962890625;
+    end
+
+    function automatic int msb_index(input int value);
+        for (int i = 31; i >= 0; i--) begin
+            if (value[i]) begin
+                return i;
+            end
+            // NOTE: no need for else bc we already check for d_reg > 0
+        end
+    endfunction
 
 endmodule
